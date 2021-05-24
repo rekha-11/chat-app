39f7bf94c741dd925eb969358f3f4c908c48b210
Rekha Bhandari <rekhabhandaree77@gmail.com>
2021-05-24 17:38:27 +0545

    ok

--- src/Components/ChatFeed.js
+++ src/Components/ChatFeed.js
@@ -6,51 +6,66 @@ import MyMessage from './MyMessage'
 
 export default function ChatFeed(props) {
 
-    const {chats, activechats, userName, messages} = props;
+    const {chats, activeChats, userName, messages} = props;
 
-    const chat = chats && chats[activechats]
+    const chat = chats && chats[activeChats]
 
     const renderMessages =() => {
         const keys = Object.keys(messages);
         
         return keys.map((key, index) =>{
             const message = messages[key];
-            const lastMessageKey = index ===0? null: keys[index-1];
-            const isMyMessage = userName ===message.sender.userName
+            const lastMessageKey = index === 0? null: keys[index-1];
+            const isMyMessage = userName ===message.sender.username
+            
             return(
                 <div key = {`msg_${index}`} style ={{width: '100%'}}>
                     <div className= 'message-block'>
                         {
-                            isMyMessage ? 
-                            <MyMessage message={message}/> : 
+                            isMyMessage 
+                            ? <MyMessage message={message}/> : 
                             <TheirMessage message={message} lastMessage={messages[lastMessageKey]}/>
                         }
                     </div>
                     <div className = 'read-receipts' style = {{marginRight: isMyMessage? '18px': '0px',
-                    marginLeft : isMyMessage ? '18px': '68px'
-                    }}>
+                    marginLeft : isMyMessage ? '18px': '68px'}}>
                     </div>
                 </div>
             )
         })
     }
 
-    if(!chat) return 'Loading...';
-
+    if(!chats) return 'Loading...'
     return (
         <div className='chat-feed'>
             <div className = 'chat-title-container'>
-                <div className = 'chat-title'>{chat.itle}</div>
+                <div className = 'chat-title'>{chat?.title}</div>
                 <div className = 'chat-subtitle'>
                     {chat.people.map((person) => `${person.person.username}`)}
                 </div>
             </div>
             {renderMessages()}
             <div style ={{height:'100px'}} />
-            <div className='message-form-container'>
-                <MessageForm {...props} chatID = {activechats}/>
+            <div className='message-form-container'>Hello
+                <MessageForm {...props} chatID = {activeChats}/>
             </div>
             
         </div>
+        
     )
 }
+
+{/* <div className='chat-feed'>
+            <div className = 'chat-title-container'>
+                <div className = 'chat-title'>{chat?.title}</div>
+                <div className = 'chat-subtitle'>
+                    {chat.people.map((person) => `${person.person.userName}`)}
+                </div>
+            </div>
+            {renderMessages()}
+            <div style ={{height:'100px'}} />
+            <div className='message-form-container'>Hello
+                <MessageForm {...props} chatID = {activechats}/>
+            </div>
+            
+        </div> */}
